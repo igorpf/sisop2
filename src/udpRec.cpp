ed89358daaee9186ef834beb532abee3d040779b
Igor Ferreira <ipferreira@inf.ufrgs.br>
2018-03-31 15:24:07 -0300

    Refactoring a little bit

--- src/udpRec.cpp
+++ src/udpRec.cpp
@@ -1,36 +1,43 @@
-
 #include <iostream>
 #include <fstream>
 
 #include <stdlib.h>
 #include <string.h>
-
-#define SOCKET int
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+
+#define SOCKET int
 #define BUFFER_SIZE 64000
+/*
+ * struct timeval read_timeout;
+    read_timeout.tv_sec = 0;
+    read_timeout.tv_usec = 10;
+    read_timeout.tv_usec = 5*1000*1000;
+    setsockopt(s, SOL_SOCKET, SO_RCVTIMEO, &read_timeout, sizeof read_timeout);
+ * */
+
 
 int main(int argc, char **argv){
     struct  sockaddr_in peer;
     SOCKET  s;
-    int porta, peerlen, rc, i;
+    int port, peerlen, rc;
     std::string outPath;
 
     if(argc < 5) {
-        std::cout << "Utilizar:" << std::endl;
-        std::cout << "rec -p <porta> -o <arq>" << std::endl;
+        std::cout << "Use:" << std::endl;
+        std::cout << "rec -p <port> -o <outFile>" << std::endl;
         exit(1);
     }
 
-    for(i=1; i < argc; i++) {
+    for(int i=1; i < argc; i++) {
         if(argv[i][0]=='-'){
             switch(argv[i][1]){
                 case 'p':
                     i++;
-                    porta = atoi(argv[i]);
-                    if(porta < 1024) {
-                        std::cout << "Porta invalida" << std::endl;
+                    port = atoi(argv[i]);
+                    if(port < 1024) {
+                        std::cout << "Invalid port" << std::endl;
                         exit(1);
                     }
                     break;
@@ -39,59 +46,52 @@ int main(int argc, char **argv){
                     outPath.assign(argv[i], sizeof(argv[i]));
                     break;
                 default:
-                    std::cout << "Parametro invalido " << i << ": " << argv[i] << std::endl;
+                    std::cout << "Invalid parameter " << i << ": " << argv[i] << std::endl;
                     exit(1);
             }
-        }else{
-            std::cout << "Parametro invalido " << i << ": " << argv[i] << std::endl;
+        } else {
+            std::cout << "Invalid parameter " << i << ": " << argv[i] << std::endl;
             exit(1);
         }
     }
 
-// Cria o socket na familia AF_INET (Internet) e do tipo UDP (SOCK_DGRAM)
     if ((s = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        std::cout << "Falha na criacao do socket" << std::endl;
+        std::cout << "Error creating socket" << std::endl;
         exit(1);
     }
 
-// Define domínio, IP e porta a receber dados
     memset((void *) &peer,0,sizeof(struct sockaddr_in));
     peer.sin_family = AF_INET;
-    peer.sin_addr.s_addr = htonl(INADDR_ANY); // Recebe de qualquer IP
-    peer.sin_port = htons(porta); // Recebe na porta especificada na linha de comando
+    peer.sin_addr.s_addr = htonl(INADDR_ANY); 
+    peer.sin_port = htons(port);
     peerlen = sizeof(peer);
 
-// Associa socket com estrutura peer
     if(bind(s,(struct sockaddr *) &peer, peerlen)) {
-        std::cout << "Erro no bind" << std::endl;
+        std::cout << "Bind error" << std::endl;
         exit(1);
     }
 
-//    struct timeval read_timeout;
-//    read_timeout.tv_sec = 0;
-//    read_timeout.tv_usec = 10;
-//    read_timeout.tv_usec = 5*1000*1000;
-//    setsockopt(s, SOL_SOCKET, SO_RCVTIMEO, &read_timeout, sizeof read_timeout);
-
-    std::cout << "Socket inicializado. Aguardando mensagens..." << std::endl << std::endl;
+    std::cout << "Initialized socket " << std::endl << std::endl;
     char buffer[BUFFER_SIZE];
-    // Recebe pacotes do cliente e responde com string "ACK"
+
     std::string ack("ack");
     std::ofstream outputFile;
     outputFile.open(outPath.c_str());
-    if(outputFile.is_open()) {
-        do {
-            memset(&buffer,0,sizeof(buffer));
-//            std::cout << "Antes do receive: " << buffer << std::endl;
-            rc = recvfrom(s,buffer,sizeof(buffer),0,(struct sockaddr *) &peer,(socklen_t *)&peerlen);
-            outputFile << buffer << std::endl;
-//            std::cout << "Recebido " << buffer << std::endl << std::endl << std::endl;
-            std::cout << "Recebido mais um pacote" << std::endl << std::endl << std::endl;
-            sendto(s,ack.c_str(),ack.length(),0,(struct sockaddr *)&peer, peerlen);
-        } while (rc > 0 && buffer[0] != '\0');
-        outputFile.close();
-    } else {
-
+    if(!outputFile.is_open()) {
+        std::cout << "Could not receive file!" << std::endl;
+        exit(1);
     }
+
+    do {
+        memset(&buffer,0,sizeof(buffer));
+//        std::cout << "Antes do receive: " << buffer << std::endl;
+        rc = recvfrom(s,buffer,sizeof(buffer),0,(struct sockaddr *) &peer,(socklen_t *)&peerlen);
+        outputFile << buffer << std::endl;
+//        std::cout << "Recebido " << buffer << std::endl << std::endl << std::endl;
+        std::cout << "Recebido mais um pacote" << std::endl << std::endl << std::endl;
+        sendto(s,ack.c_str(),ack.length(),0,(struct sockaddr *)&peer, peerlen);
+    } while (rc > 0 && buffer[0] != '\0');
+    outputFile.close();
+
 
 }
--- src/udpTr.cpp
+++ src/udpTr.cpp
@@ -11,40 +11,38 @@
 #include <arpa/inet.h>
 
 #define SOCKET  int
-#define BUFFER_SIZE 64000 //aproximately an ip packet size
+#define BUFFER_SIZE 64000 //approximately an ip packet size
 
 int main(int argc, char **argv){
     struct sockaddr_in peer;
-    SOCKET s;
+    SOCKET sock;
     double rate = 1.0;
-    int porta, peerlen, rc, i;
-    char ip[16];
+    int port, peerlen;
+    std::string ip;
     std::string filePath;
 
     if(argc < 9) {
-        std::cout << "Utilizar: (argc " << argc << ")" << std::endl;
-        std::cout << "trans -h <numero_ip> -p <porta> -r <rate> -f <arquivo>" << std::endl;
+        std::cout << "Use: (argc " << argc << ")" << std::endl;
+        std::cout << "trans -h <ip_number> -p <port> -r <rate> -f <file>" << std::endl;
         exit(1);
     }
 
-    // Pega parametros
-    for(i = 1; i < argc; i++) {
+    for(int i = 1; i < argc; i++) {
         if(argv[i][0]=='-') {
             switch(argv[i][1]) {
-                case 'h': // Numero IP
+                case 'h':
                     i++;
-                    strcpy(ip, argv[i]);
+                    ip.assign(argv[i], strlen(argv[i]));
                     break;
-                case 'f': // Numero IP
+                case 'f':
                     i++;
-                    filePath.assign(argv[i], sizeof(argv[i]));
-//                    std::cout << filePath << std::endl;
+                    filePath.assign(argv[i], strlen(argv[i]));
                     break;
-                case 'p': // porta
+                case 'p':
                     i++;
-                    porta = atoi(argv[i]);
-                    if(porta < 1024) {
-                        std::cout << "Valor da porta invalido" << std::endl;
+                    port = atoi(argv[i]);
+                    if(port < 1024) {
+                        std::cout << "Invalid port" << std::endl;
                         exit(1);
                     }
                     break;
@@ -52,30 +50,28 @@ int main(int argc, char **argv){
                     i++;
                     rate = atoi(argv[i]) * 1000.0;
                     if(rate < 0) {
-                        std::cout << "Valor de velocidade invalido" << std::endl;
+                        std::cout << "Invalid rate parameter" << std::endl;
                         exit(1);
                     }
                     break;
                 default:
-                    std::cout << "Parametro invalido " << i << ": " << argv[i] << std::endl;
+                    std::cout << "Invalid parameter " << i << ": " << argv[i] << std::endl;
                     exit(1);
             }
         } else {
-            std::cout << "Parametro invalido " << i << ": " << argv[i] << std::endl;
+            std::cout << "Invalid parameter " << i << ": " << argv[i] << std::endl;
             exit(1);
         }
     }
 
-// Cria o socket na familia AF_INET (Internet) e do tipo UDP (SOCK_DGRAM)
-    if((s = socket(AF_INET, SOCK_DGRAM,0)) < 0) {
-        std::cout << "Falha na criacao do socket" << std::endl ;
+    if((sock = socket(AF_INET, SOCK_DGRAM,0)) < 0) {
+        std::cout << "Error creating socket" << std::endl ;
         exit(1);
     }
 
-// Cria a estrutura com quem vai conversar
     peer.sin_family = AF_INET;
-    peer.sin_port = htons(porta);
-    peer.sin_addr.s_addr = inet_addr(ip);
+    peer.sin_port = htons(port);
+    peer.sin_addr.s_addr = inet_addr(ip.c_str());
     peerlen = sizeof(peer);
 
     int sleepTime = (1000000.0/(rate/100000.0));
@@ -91,24 +87,24 @@ int main(int argc, char **argv){
     inputFile.seekg(0, inputFile.end);
     size_t fileLength = inputFile.tellg();
     inputFile.seekg(0, inputFile.beg);
+    int packets = ceil(fileLength/((float) BUFFER_SIZE)), receivedBytes;
+
     char buffer[BUFFER_SIZE+1];
-    int packets = ceil(fileLength/((float) BUFFER_SIZE));
-    std::cout << "Dividido em " << packets << " pacotes. File size: " << fileLength <<  std::endl;
+    std::cout << "Divided in " << packets << " packets. File size: " << fileLength <<  std::endl;
+
     while(packets--) {
         memset(&buffer,0,sizeof(buffer));
         inputFile.read(buffer, sizeof(buffer));
         buffer[BUFFER_SIZE] = '\0';
 //        std::cout << buffer << std::endl << " sz of " << sizeof(buffer) << std::endl;
-        sendto(s, buffer, sizeof(buffer), 0, (struct sockaddr *)&peer, peerlen);
+        sendto(sock, buffer, sizeof(buffer), 0, (struct sockaddr *)&peer, peerlen);
         usleep(sleepTime);
-        rc = recvfrom(s,ack,sizeof(ack),0,(struct sockaddr *) &peer,(socklen_t *)&peerlen);
+        receivedBytes = recvfrom(sock, ack, sizeof(ack), 0,(struct sockaddr *) &peer,(socklen_t *)&peerlen);
         ack[3] = '\0';
-        std::cout << ack << " veio pra ser ack, bits recebidos: " << rc << std::endl;
+        std::cout << ack << " received" << std::endl;
     }
     buffer[0] = '\0';
-    sendto(s, buffer, 1, 0, (struct sockaddr *)&peer, peerlen);
+    sendto(sock, buffer, 1, 0, (struct sockaddr *)&peer, peerlen);
     inputFile.close();
-
-
 
 }
