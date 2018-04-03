f208b84064fd234ff2ff318e95bff7a6cc628f66
jfguimaraes <jefersonfguimaraes@gmail.com>
2018-04-02 22:54:31 -0300

    Ajustados CMakes de testes para linkar os fontes que estão sendo testados

--- util/test/util_test.cpp
+++ util/test/util_test.cpp
@@ -2,24 +2,18 @@
 
 #include <gtest/gtest.h>
 
-TEST(Foo, Bar)
-{
-    ASSERT_TRUE(true);
-}
-
-TEST(Foo, Bar1)
+TEST(FileTransfer, ReceiveAndSend)
 {
     file_transfer_request request;
 //    request.ip = "192.168.0.11";
     request.ip = "127.0.0.1";
     request.transfer_rate = 1000;
-    request.port=9000;
-    request.in_file_path="files/in_ascii.txt";
-    request.out_file_path="files/out_ascii.txt";
+    request.port = 9000;
+    request.in_file_path = "files/in_ascii.txt";
+    request.out_file_path = "files/out_ascii.txt";
     receive_file(request);
     send_file(request);
 }
-
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
